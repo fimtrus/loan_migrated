80590548a11cd78d1a6390ed59d570c96cebfd2e
fimtrus <fimtrus@gmail.com>
2016-03-16 14:53:04 +0900

    remove file that not used

--- src/com/fimtrus/loan/model/Constant.java
+++
@@ -1,8 +0,0 @@
-package com.fimtrus.loan.model;
-
-public class Constant {
-	public static final String EXTRA_LOANS = "extra_loans";
-	public static final String EXTRA_INTEREST_RATE = "extra_interest_rate";
-	public static final String EXTRA_TERM = "extra_term";
-	public static final String EXTRA_SELECTED_INDEX = "extra_selected_index";
-}
